92135d16577d98ab9c528cffe2168466ddd5ab6f
Camily <cavicfina@gmail.com>
2025-04-30 22:48:34 -0300

    Trabalho versão final

--- Algoritmos II/Trabalhos/Trabalho 02/trabalho02.py
+++ Algoritmos II/Trabalhos/Trabalho 02/trabalho02.py
@@ -1,7 +1,9 @@
+#Alunos: Camily Victal Finamor - 2024001197 / Luis Gustavo Riso Santos - 2024002372
+#Trabalho 2 da disciplina Algoritmos e Programacao II
+#------------------------------------------------------------------------------------------------------------------------------------------
 import random
 
-#Leitura dos animais + pontuação dos seus atributos
-
+#Funcao de leitura do arquivo csv, onde inserimos as criaturas em um dicionario
 def leituraArquivo(brainrot, criaturas):
     with open(brainrot, 'r', encoding='utf-8') as arquivo:
         linhas = arquivo.readlines()
@@ -11,40 +13,35 @@ def leituraArquivo(brainrot, criaturas):
             nome = criaturaAtual[0]
             atributos = list(map(float, criaturaAtual[1:]))
 
-            criatura = {
-                "nome": nome,
+            criaturas[nome] = {
                 "atributos": atributos
             }
-            criaturas.append(criatura)
 
-#----------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------
 
-#Funcao de leitura do pesos + soma das notas por animal
-
+#Funcao de leitura dos pesos do usuario, onde ja fazemos o calculo da soma da nota do animal multiplicado pelo peso de acordo com o usuario
 def leituraPesos(usuarios, quantidadeUsuarios, criaturas):
     i = 0
     while(i < quantidadeUsuarios):
         usuario = list(map(int, (input("").split())))
-        #usuarios.append(usuario) #vetor com 5 pesos []
-        for j in range(0, len(criaturas), 1):
+        for nomeAnimal, criatura in criaturas.items():
             somaPesos = 0
             for k in range(6):
-                somaPesos += usuario[k] * criaturas[j]["atributos"][k]
-            vetor = [criaturas[j]["nome"], somaPesos]
+                somaPesos += usuario[k] * criatura["atributos"][k]
+            vetor = [nomeAnimal, somaPesos]
             usuarios[i].append(vetor)
         i += 1
 
-#----------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------
 
-#Algoritmo de ordenação para a parte A
-
+#Algoritmo de ordenacao para a parte A: Quick-sort
 def partition(lista, esq, dir):
     r = random.randint(esq, dir)
     aux = lista[r]
     lista[r] = lista[esq]
     lista[esq] = aux
     j = esq
-    pivot = lista[esq] #vetor[nome, nota, -1]
+    pivot = lista[esq] 
     for k in range(esq+1, dir+1, 1):
         if lista[k][1] < pivot[1]: 
             j += 1
@@ -60,70 +57,82 @@ def partition(lista, esq, dir):
     lista[j] = pivot
     return j
 
-def quicksort(listaUsuario, esq, dir): #recebe os indices de onde comeca a lista e onde termina a lista | indices inclusive
-    if esq < dir:
-        p = partition(listaUsuario, esq, dir) #descobre a posicao que o p (pivot) estah
-        quicksort(listaUsuario, esq, p-1) #puxa a lista da esquerda e o proprio pivot ja estah certo
-        quicksort(listaUsuario, p+1, dir) #puxa a lista da direita
+def quicksort(listaUsuario, esq, dir):
+        if esq < dir:
+            p = partition(listaUsuario, esq, dir) 
+            quicksort(listaUsuario, esq, p-1) 
+            quicksort(listaUsuario, p+1, dir) 
     
-#----------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------
 
-#Funcao para ranquear animais
-
+#Funcao para unificar a preferencia dos usuarios e calcular a soma dessas preferencias para cada animal
 def ranquearAnimais(usuario, criaturas):
+    max = 0
     for i in range(len(usuario)):
         usuario[i][1] = i
-        for j in range(len(criaturas)):
-            if usuario[i][0] == criaturas[j]["nome"]:
-                if isinstance(criaturas[j]["atributos"], list):
-                    criaturas[j]["atributos"] = 0
-                criaturas[j]["atributos"] += usuario[i][1]
-                
-#----------------------------------------------------------------------------------------------------------------------
+        #sobrescrevemos a lista de atributos pelo valor da soma final de acordo com as preferencias dos usuarios
+        if isinstance(criaturas[usuario[i][0]]["atributos"], list):
+            criaturas[usuario[i][0]]["atributos"] = 0
+        criaturas[usuario[i][0]]["atributos"] += usuario[i][1]
+        if max < criaturas[usuario[i][0]]["atributos"]:
+            max = criaturas[usuario[i][0]]["atributos"]
+    return max
 
-#Funcao para ranquear animais
+#------------------------------------------------------------------------------------------------------------------------------------------
 
-def StableCountingSort(lista, k):
+#Algoritmo de ordenacao para a parte B: Couting-sort (versao estavel)
+def StableCountingSort(lista, maiorPontuacaoCriatura):
     B = [None for _ in range(0, len(lista))]
-    C = [0 for _ in range(0, k+1)]
+    C = [0 for _ in range(0, maiorPontuacaoCriatura+1)]
 
     for a in lista: 
-        C[a["atributos"]] += 1 
+        C[a[1]] += 1 
 
-    for i in range(1, k+1):
-        C[i] = C[i] + C[i-1]
-
+    for i in range(1, maiorPontuacaoCriatura+1):
+        C[i] = C[i] + C[i-1] 
+    
     for i in range(len(lista)-1, -1, -1):  
         a = lista[i]
-        B[C[a["atributos"]] - 1] = a
-        C[a["atributos"]] -= 1 
+        B[C[a[1]] - 1] = a
+        C[a[1]] -= 1 
+
+    #Looping pque faz a verificacao de repeticoes e que ordena alfabeticamente caso a repeticao aconteca
+    for i in range(1, len(B)):
+        if B[i][1] == B[i - 1][1]:  
+            j = i
+            while j > 0 and B[j][1] == B[j - 1][1] and B[j][0] > B[j - 1][0]:
+                B[j], B[j - 1] = B[j - 1], B[j]
+                j -= 1
     
     return B
 
-#----------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------------------------
+#MAIN
 
+#No main fizemos a leitura de entrada com o nome do arquivo e a quantidade de usuarios
+#E criamos o dicionario de criaturas e a lista de usuarios
 if __name__ == "__main__":
     nomeArquivo = input()
     quantidadeUsuarios = int(input())
-    criaturas = []
+    criaturas = {}
     usuarios = [[] for _ in range(quantidadeUsuarios)] 
     leituraArquivo(nomeArquivo, criaturas)
-    #print(criaturas)
     leituraPesos(usuarios, quantidadeUsuarios, criaturas)
-    #for i in range(len(usuarios)):
-    #    print(usuarios[i])
-    #print("\n\n")
+
+#Ordenamos de acordo com o ranking pessoal dos usuarios e ja fazemos a soma da unificacao de preferencias dos usuarios para os animais
     for i in range(len(usuarios)):
         quicksort(usuarios[i], 0, len(usuarios[i]) - 1)
-    #for i in range(len(usuarios)):
-    #    print(usuarios[i])
-    #print("\n\n")
-        ranquearAnimais(usuarios[i], criaturas)
-    #for i in range(len(usuarios)):
-    #    print(usuarios[i])
-    #print("\n\n")
-    k = max(c['atributos'] for c in criaturas)
-    criaturas = StableCountingSort(criaturas, k)
-    for i in range(len(criaturas)-1, -1, -1):
-        print(f"{criaturas[i]['nome']}: {criaturas[i]['atributos']}")
+        maiorPontuacaoCriatura = ranquearAnimais(usuarios[i], criaturas)
+
+#e para o ranking final das criaturas no geral, criamos um vetor auxiliar que sera usado no algoritmo de ordenacao, ja que nossas criaturas estavam
+#em um dicionario. Cada slot do nosso vetor contem uma tupla que guarda o nome do animal e a soma da unificacao de preferencias dos usuarios para os animais
+    listacriaturas = []
+    for nomeAnimal, criatura in criaturas.items():
+        dadoCriatura = (nomeAnimal, int(criatura["atributos"]))
+        listacriaturas.append(dadoCriatura)
+    listacriaturas = StableCountingSort(listacriaturas, maiorPontuacaoCriatura)
+
+#Por fim, imprimimos essas tuplas de animais, exibindo seu nome e sua nota
+    for i in range(len(listacriaturas)-1, -1, -1):
+        print(f"{listacriaturas[i][0]} {listacriaturas[i][1]}")
 
