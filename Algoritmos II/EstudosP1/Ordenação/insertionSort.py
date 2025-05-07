00bd86f8d339c07b2d6da3b1a81d760bceb00050
Camily <cavicfina@gmail.com>
2025-05-07 09:20:55 -0300

    insertionSort

--- Algoritmos II/EstudosP1/Ordenação/insertionSort.py
+++ Algoritmos II/EstudosP1/Ordenação/insertionSort.py
@@ -1,10 +1,11 @@
 def insertionSort(lista):
     for i in range(1, len(lista), 1): #meu for que controla a parte não ordenada, começa em 1 pq assumimos que o primeiro elemento lista[0] sozinho está ordenado
-        for j in range(0, i): #meu for que controla a parte ordenada
-            if lista[j][1] > lista[i][1]: 
-                lista[j], lista[i] = lista[i], lista[j] #faço a troca
-                print(lista)
-
+        valorInserir = lista[i]
+        indiceAnterior = i
+        while indiceAnterior > 0 and lista[indiceAnterior-1][1] > valorInserir[1]: #meu looping que controla a parte ordenada
+            lista[indiceAnterior] = lista[indiceAnterior - 1]
+            indiceAnterior -= 1
+        lista[indiceAnterior] = valorInserir
 
 #testando o insertion sort
 lista = [('camily', 2), ('jose', 2), ('pedro', 2), ('rafael', 1)]
